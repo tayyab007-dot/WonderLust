2834c27df0ec71dbeee16921daa8dd39caa10b7e
tayyab007-dot <chughtai22556@gmail.com>
2026-07-08 13:07:33 +0500

    Implement The passport and library setup for authentication and authorization basic setup

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utlls/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+
 // const { listingSchema, reviewSchema } = require("./schema.js");
 // const Review = require("./models/review.js");
 
@@ -48,6 +52,13 @@ app.get("/", (req, res) => {
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
