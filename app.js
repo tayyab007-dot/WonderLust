576245c53e9cec4c6fef0d23f6ac3fe3adfd1630
tayyab007-dot <chughtai22556@gmail.com>
2026-07-15 12:13:19 +0500

    Today I add some new features add to home/index.ejs add fileters, toggle button and upgrade navbar and all features are responsive

--- app.js
+++ app.js
@@ -54,9 +54,9 @@ const sessionOptions = {
   },
 };
 
-app.get("/", (req, res) => {
-  res.send("Root is working");
-});
+// app.get("/", (req, res) => {
+//   res.send("Root is working");
+// });
 
 app.use(session(sessionOptions));
 app.use(flash());
